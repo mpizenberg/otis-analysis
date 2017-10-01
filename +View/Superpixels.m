f36e78f779af318e490adb63fb8c6c2f080948eb
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-10-01 21:55:57 +0800

    Change one superpixels visualization

--- +View/Superpixels.m
+++ +View/Superpixels.m
@@ -4,10 +4,19 @@ classdef (Abstract) Superpixels
 methods (Static)
 
 
-function [ mask ] = boundaries ( superpixels )
+function [ mask ] = boundariesMask ( superpixels )
 	gx = logical( superpixels - superpixels( :, [2:end, end] ) );
 	gy = logical( superpixels - superpixels( [2:end, end], : ) );
 	mask = gx | gy;
+end
+
+
+function [ visualization ] = boundaries ( superpixels, bg_img, colorRGB )
+	mask = View.Superpixels.boundariesMask( superpixels );
+	R = bg_img(:,:,1); R(mask) = colorRGB(1);
+	G = bg_img(:,:,2); G(mask) = colorRGB(2);
+	B = bg_img(:,:,3); B(mask) = colorRGB(3);
+	visualization = cat( 3, R, G, B );
 end
 
 
